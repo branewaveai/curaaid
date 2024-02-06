982d4f14ec0e233cace072ff31d15ad675f96ceb
Vijay Maurya <vijaymaurya912082@gmail.com>
2024-02-07 00:50:25 +0530

    head landing layout

--- src/components/homePage/topPage.js
+++ src/components/homePage/topPage.js
@@ -1,4 +1,3 @@
-// File: TopPage.js
 import React from 'react';
 import styled from 'styled-components';
 import CoverImage2 from "../../assets/icons/CoverImage2.png";
@@ -6,73 +5,88 @@ import coverImage1 from "../../assets/icons/coverImage1.png";
 
 const Wrapper = styled.div`
   display: grid;
-  grid-template-columns: 1fr 1fr; /* Equal width columns */
+  grid-template-columns: 1fr 1fr; 
   max-width: 100%;
   margin: 0;
 `;
-
 const BlockContainer = styled.div`
-  margin: 10px; /* Add margin to create space around the blocks */
+  margin: 5px; 
 `;
 
 const ImageBlock = styled(BlockContainer)`
   width: 100%;
   height: 300px;
   display: flex;
+  border: 1px solid #ddd;
   align-items: center;
   justify-content: center;
-  // border: 1px solid #ddd;
-  border-radius: 15px; /* Optional: Add border-radius for a rounded appearance */
-  margin-right: 40px; /* Add margin-bottom for spacing */
+  margin-bottom:0px;
+  border-radius: 10px; 
+  // margin-right: 400px; 
 `;
-
 const ImageContent = styled.img`
   width: 100%;
-  height: 100%; /* Ensure the image takes up the entire space */
+  height: 100%;
   object-fit: cover;
-  border-radius: 20px; /* Optional: Add border-radius for a rounded appearance */
-  &.img {
-    /* Add specific styles for the img element */
-  }
+  border-radius: 15px; 
 `;
 
 const FlexContainer = styled.div`
   display: flex;
-  justify-content: space-between;
-  height: 100px;
-  // border: 1px solid #ddd;
-  border-radius: 15px; /* Optional: Add border-radius for a rounded appearance */
-  margin-bottom: 20px; /* Add margin-bottom for spacing */
+  justify-content: normal;
+  height: 160px;
+  border-radius: 15px; 
+  margin-bottom: 20px; 
 `;
 
 const Block = styled(BlockContainer)`
-  width: 50%; /* Equal width columns */
-  height: 340px;
+  border: 1px solid #ddd;
+  width: 50%;
+  height: 330px; 
   display: flex;
+  flex-direction: column;
   align-items: center;
-  justify-content: center;
-  margin-left :20px;
-  
-  // border: 1px solid red;
-  border-radius: 15px; /* Optional: Add border-radius for a rounded appearance */
+  justify-content: normal; 
+  border-radius: 15px; 
+  margin-left:10px;
 `;
 
-const Block1 = styled(Block)`
-  height: 320px; /* Adjust the height of Block1 */
-  // border: 1px solid red;
-  width:95%;
+const HalfBlock = styled(BlockContainer)`
+  width: 300px;
+  height: 160px;
+  display: flex;
+  align-items: center;
+  border: 1px solid #ddd;
+  justify-content: center;
+  border-radius: 15px; 
 `;
 
 const ImageCover = styled(BlockContainer)`
   width: 95%;
-  height: 280px; /* Adjust the height of the cover image */
-  // border: 1px solid #bbb;
+  height: 280px;
   display: flex;
   align-items: center;
   justify-content: center;
-  margin-left:20px;
-  margin-top:40px;
-  border-radius: 15px; /* Optional: Add border-radius for a rounded appearance */
+  margin-left: 20px;
+  margin-top: 50px;
+  border-radius: 10px; 
+`;
+const Block1 = styled(BlockContainer)`
+  border: 1px solid #ddd;
+  width: 99%;
+  height: 160px; 
+  border-radius: 10px; 
+  display: flex;
+  justify-content: normal;
+`;
+
+const QuarterBlock = styled(BlockContainer)`
+  width: 50%; 
+  height: 140px;
+  border: 1px solid #ddd;
+  margin-bottom:0px;
+  margin-top:0px;
+  border-radius: 10px; 
 `;
 
 const TopPage = () => {
@@ -81,13 +95,24 @@ const TopPage = () => {
       <ImageBlock>
         <ImageContent src={coverImage1} alt="Image" className="img" />
       </ImageBlock>
+
       <FlexContainer>
-        <Block>Random Text</Block>
-        <Block>Random Text</Block>
+        <Block>Another Block</Block>
+        <div>
+          <HalfBlock>Random Text</HalfBlock>
+          <HalfBlock>Random Text</HalfBlock>
+        </div>
       </FlexContainer>
-      <Block1>Random Text</Block1>
+      <div>
+      <div style={{display:'flex' , margin: '10px 0'}}>
+        <QuarterBlock>Random Text</QuarterBlock>
+        <QuarterBlock>Random Text</QuarterBlock>
+      </div>
+       <Block1>Random Text</Block1>
+      </div>
+      
       <ImageCover>
-      <ImageContent src={CoverImage2} alt="Image" className="img" />
+        <ImageContent src={CoverImage2} alt="Image" className="img" />
       </ImageCover>
     </Wrapper>
   );
